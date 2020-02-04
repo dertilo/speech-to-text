ba50b33fb947f4dd5c0db813958d5ffcce7b0f8f
Tilo Himmelsbach <tilo.himmelsbach@tu-berlin.de>
2020-02-04 19:25:54 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    reqs = f.read()
+
+setup(
+    name='speechtotext',
+    version='0.1',
+    packages=find_packages(include=["corpora"]),
+    license='LICENSE.txt',
+    long_description=open('README.md').read(),
+    install_requires=reqs.strip().split('\n'),
+)
