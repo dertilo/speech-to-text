72e82015c68f0c8c6004ce3453c4136dafd9371d
Tilo Himmelsbach <dertilo@gmail.com>
2020-04-04 19:23:38 +0200

    librispeech.py

--- corpora/librispeech.py
+++ corpora/librispeech.py
@@ -1,17 +1,14 @@
 import os
-from os.path import join
 from pathlib import Path
+from typing import Tuple
 
-import torchaudio
-from tqdm import tqdm
 from util import data_io
 
 HOME = os.environ["HOME"]
 
 
-def generate_audiofile_text_tuples(raw_data_path, folder):
-
-    p = Path(join(raw_data_path, folder))
+def generate_audiofile_text_tuples(path:str)->Tuple[str,str]:
+    p = Path(path)
     audio_files = list(p.rglob("*.flac"))
 
     def parse_line(l):
@@ -34,31 +31,3 @@ def generate_audiofile_text_tuples(raw_data_path, folder):
             text = key2utt[k]
             audio_file = str(f)
             yield audio_file, text
-
-
-MILLISECONDS_TO_SECONDS = 0.001
-
-
-def get_length(audio_file):
-    si, ei = torchaudio.info(audio_file)
-    length_ms = int(si.length / si.channels / si.rate / MILLISECONDS_TO_SECONDS)
-    return length_ms
-
-
-if __name__ == "__main__":
-
-    asr_path = HOME + "/data/asr_data"
-    raw_data_path = asr_path + "/ENGLISH/LibriSpeech"
-
-    for folder in next(os.walk(raw_data_path))[1]:
-        print(folder)
-        g = generate_audiofile_text_tuples(raw_data_path, folder)
-        file_utt_g = (
-            {
-                "audio-file": audio_file,
-                "text": text,
-                "length_ms": get_length(audio_file),
-            }
-            for audio_file, text in tqdm(g)
-        )
-        data_io.write_jsonl(join(raw_data_path, folder + "_manifest.jsonl"), file_utt_g)
