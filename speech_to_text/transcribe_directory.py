1d210825863c5b9aafd423ef544ca051e169b8cd
Tilo Himmelsbach <dertilo@gmail.com>
2021-08-15 10:26:29 +0200

    english and spanish

--- speech_to_text/transcribe_directory.py
+++ speech_to_text/transcribe_directory.py
@@ -20,45 +20,54 @@ from speech_to_text.transcribe_audio import (
     TARGET_SAMPLE_RATE,
 )
 
-
+lang2model = {
+    "english": "jonatasgrosman/wav2vec2-large-xlsr-53-english",
+    "spanish": "jonatasgrosman/wav2vec2-large-xlsr-53-spanish",
+}
 if __name__ == "__main__":
 
     import subprocess
 
     video_dir = sys.argv[1]
-    asr = SpeechToText(
-        model_name="jonatasgrosman/wav2vec2-large-xlsr-53-spanish",
-    ).init()
-    output_dir = "transcripts"
-    if os.path.isdir(output_dir):
-        shutil.rmtree(output_dir)
-    os.makedirs(output_dir)
 
-    for video_file in Path(video_dir).glob("*.mp4"):
-        file_name = video_file.stem
-        with NamedTemporaryFile(suffix=".wav", delete=True) as tmp_file:
-            command = f"ffmpeg -y -i '{video_file}' -ab 160k -ac 1 -ar 16000 -vn {tmp_file.name}"  #
-            subprocess.call(command, shell=True)
+    for lang in lang2model.keys():
 
-            audio = AudioSegment.from_file(
-                tmp_file.name,
-                target_sr=TARGET_SAMPLE_RATE,
-                offset=0.0,
-                trim=False,
+        output_dir = f"transcripts/{lang}"
+        if os.path.isdir(output_dir):
+            shutil.rmtree(output_dir)
+        os.makedirs(output_dir)
+
+        asr = SpeechToText(
+            model_name=lang2model[lang],
+        ).init()
+
+        video_lang_dir=f"{video_dir}/{lang}"
+        print(video_lang_dir)
+        for video_file in Path(video_lang_dir).glob("*.mp4"):
+            file_name = video_file.stem
+            with NamedTemporaryFile(suffix=".wav", delete=True) as tmp_file:
+                command = f"ffmpeg -y -i '{video_file}' -ab 160k -ac 1 -ar 16000 -vn {tmp_file.name}"  #
+                subprocess.call(command, shell=True)
+
+                audio = AudioSegment.from_file(
+                    tmp_file.name,
+                    target_sr=TARGET_SAMPLE_RATE,
+                    offset=0.0,
+                    trim=False,
+                )
+            step = round(TARGET_SAMPLE_RATE * 10)
+            arrays = generate_arrays(audio.samples, step)
+            aligned_transcripts = [
+                (idx, asr.transcribe_audio_array(array, TARGET_SAMPLE_RATE))
+                for idx, array in arrays
+            ]
+            transcript = glue_transcripts(aligned_transcripts, step=step)
+            data_io.write_lines(
+                f"{output_dir}/{video_file.stem}.csv",
+                [f"{l.letter}\t{l.index}" for l in transcript.seq],
             )
-        step = round(TARGET_SAMPLE_RATE * 10)
-        arrays = generate_arrays(audio.samples, step)
-        aligned_transcripts = [
-            (idx, asr.transcribe_audio_array(array, TARGET_SAMPLE_RATE))
-            for idx, array in arrays
-        ]
-        transcript = glue_transcripts(aligned_transcripts, step=step)
-        data_io.write_lines(
-            f"{output_dir}/{video_file.stem}.csv",
-            [f"{l.letter}\t{l.index}" for l in transcript.seq],
-        )
 
-        data_io.write_lines(
-            f"{output_dir}/{video_file.stem}.txt",
-            ["".join([l.letter for l in transcript.seq])],
-        )
+            data_io.write_lines(
+                f"{output_dir}/{video_file.stem}.txt",
+                ["".join([l.letter for l in transcript.seq])],
+            )
