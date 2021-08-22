d0ff298be693a8e25322ea97483b3e5d7c8040fc
Tilo Himmelsbach <dertilo@gmail.com>
2021-08-22 17:44:26 +0200

    refactor

--- dash_app/updownload_app.py
+++ dash_app/updownload_app.py
@@ -9,7 +9,7 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 
 
-UPLOAD_DIRECTORY = "app_uploaded_files"
+UPLOAD_DIRECTORY = f"{os.environ['DATA_DIR']}/app_uploaded_files"
 
 if not os.path.exists(UPLOAD_DIRECTORY):
     os.makedirs(UPLOAD_DIRECTORY)
@@ -25,34 +25,6 @@ app = dash.Dash(server=server)
 def download(path):
     """Serve a file from the upload directory."""
     return send_from_directory(UPLOAD_DIRECTORY, path, as_attachment=True)
-
-
-app.layout = html.Div(
-    [
-        html.H1("File Browser"),
-        html.H2("Upload"),
-        dcc.Upload(
-            id="upload-data",
-            children=html.Div(
-                ["Drag and drop or click to select a file to upload."]
-            ),
-            style={
-                "width": "100%",
-                "height": "60px",
-                "lineHeight": "60px",
-                "borderWidth": "1px",
-                "borderStyle": "dashed",
-                "borderRadius": "5px",
-                "textAlign": "center",
-                "margin": "10px",
-            },
-            multiple=True,
-        ),
-        html.H2("File List"),
-        html.Ul(id="file-list"),
-    ],
-    style={"max-width": "500px"},
-)
 
 
 def save_file(name, content,date):
@@ -78,24 +50,4 @@ def file_download_link(filename):
     location = "/download/{}".format(urlquote(filename))
     return html.A(filename, href=location)
 
-
-@app.callback(
-    Output("file-list", "children"),
-    [Input("upload-data", "filename"), Input("upload-data", "contents")],
-)
-def update_output(uploaded_filenames, uploaded_file_contents):
-    """Save uploaded files and regenerate the file list."""
-
-    if uploaded_filenames is not None and uploaded_file_contents is not None:
-        for name, data in zip(uploaded_filenames, uploaded_file_contents):
-            save_file(name, data)
-
-    files = uploaded_files()
-    if len(files) == 0:
-        return [html.Li("No files yet!")]
-    else:
-        return [html.Li(file_download_link(filename)) for filename in files]
-
-
-if __name__ == "__main__":
-    app.run_server(debug=True, port=8888)
+# html.Li(file_download_link(filename))
