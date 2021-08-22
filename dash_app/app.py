94d2b5c3e88945399d82e55e1c5c27ddc444502d
Tilo Himmelsbach <dertilo@gmail.com>
2021-08-22 12:00:11 +0200

    refactor

--- dash_app/app.py
+++ dash_app/app.py
@@ -1,94 +1,76 @@
 import base64
 import datetime
 import io
+from pathlib import Path
 
 import dash
 from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_html_components as html
-import dash_table
 
-import pandas as pd
+from dash_app.updownload_app import save_file, uploaded_files
 
-
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-app.layout = html.Div([
-    dcc.Upload(
-        id='upload-data',
-        children=html.Div([
-            'Drag and Drop or ',
-            html.A('Select Files')
-        ]),
-        style={
-            'width': '100%',
-            'height': '60px',
-            'lineHeight': '60px',
-            'borderWidth': '1px',
-            'borderStyle': 'dashed',
-            'borderRadius': '5px',
-            'textAlign': 'center',
-            'margin': '10px'
-        },
-        # Allow multiple files to be uploaded
-        multiple=True
-    ),
-    html.Div(id='output-data-upload'),
-])
-
-
-def parse_contents(contents, filename, date):
-    content_type, content_string = contents.split(',')
-
-    decoded = base64.b64decode(content_string)
-    try:
-        if 'csv' in filename:
-            # Assume that the user uploaded a CSV file
-            df = pd.read_csv(
-                io.StringIO(decoded.decode('utf-8')))
-        elif 'xls' in filename:
-            # Assume that the user uploaded an excel file
-            df = pd.read_excel(io.BytesIO(decoded))
-    except Exception as e:
-        print(e)
-        return html.Div([
-            'There was an error processing this file.'
-        ])
-
-    return html.Div([
-        html.H5(filename),
-        html.H6(datetime.datetime.fromtimestamp(date)),
-
-        dash_table.DataTable(
-            data=df.to_dict('records'),
-            columns=[{'name': i, 'id': i} for i in df.columns]
+app.layout = html.Div(
+    [
+        dcc.Upload(
+            id="upload-data",
+            children=html.Div(["Drag and Drop or ", html.A("Select Files")]),
+            style={
+                "width": "100%",
+                "height": "60px",
+                "lineHeight": "60px",
+                "borderWidth": "1px",
+                "borderStyle": "dashed",
+                "borderRadius": "5px",
+                "textAlign": "center",
+                "margin": "10px",
+            },
+            # Allow multiple files to be uploaded
+            multiple=True,
         ),
-
-        html.Hr(),  # horizontal line
-
-        # For debugging, display the raw contents provided by the web browser
-        html.Div('Raw Content'),
-        html.Pre(contents[0:200] + '...', style={
-            'whiteSpace': 'pre-wrap',
-            'wordBreak': 'break-all'
-        })
-    ])
-
-
-@app.callback(Output('output-data-upload', 'children'),
-              Input('upload-data', 'contents'),
-              State('upload-data', 'filename'),
-              State('upload-data', 'last_modified'))
-def update_output(list_of_contents, list_of_names, list_of_dates):
-    if list_of_contents is not None:
-        children = [
-            parse_contents(c, n, d) for c, n, d in
-            zip(list_of_contents, list_of_names, list_of_dates)]
-        return children
+        html.Div(
+            id="output-data-upload",
+            children=[
+                html.Label(
+                    ["Single dynamic Dropdown", dcc.Dropdown(id="my-dynamic-dropdown")]
+                )
+            ],
+        ),
+    ]
+)
 
 
+# html.Div(
+#        children=[
+#            html.Video(
+#                controls=True,
+#                id="movie_player",
+#                src="https://www.w3schools.com/html/mov_bbb.mp4",
+#                autoPlay=True,
+#            ),
+#        ]
+#    )
+#
 
-if __name__ == '__main__':
+
+@app.callback(
+    Output("my-dynamic-dropdown", "options"),
+    Output("my-dynamic-dropdown", "value"),
+    Input("upload-data", "contents"),
+    State("upload-data", "filename"),
+    State("upload-data", "last_modified"),
+)
+def update_output(contents, names, list_of_dates):
+    if contents is not None:
+        for name, data in zip(names, contents):
+            save_file(name, data)
+    options = [{"label": Path(f).stem, "value": f} for f in uploaded_files()]
+    return options, options[0]["value"]
+
+
+if __name__ == "__main__":
     app.run_server(debug=True)
